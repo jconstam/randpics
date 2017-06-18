4edd6a16715b0e3514d83e7048c0f2036d00ba7a
Jason Constam <callmebob@gmail.com>
2017-06-18 12:03:01 +0200

    Added check on whether file was copied.

--- main_test.go
+++ main_test.go
@@ -3,24 +3,30 @@ package randpics
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
-func createTempPicFile(sourceDir string) {
+func createTempPicFile(sourceDir string) string {
 	tempFile, _ := ioutil.TempFile(sourceDir, "")
 	tempFile.Close()
 	os.Rename(tempFile.Name(), tempFile.Name()+".jpg")
+
+	return filepath.Base(tempFile.Name() + ".jpg")
 }
 
 func TestMain_CopyAllNoDelete(t *testing.T) {
 	dest, _ := ioutil.TempDir("", "")
 	source, _ := ioutil.TempDir("", "")
 
-	createTempPicFile(source)
+	file1 := createTempPicFile(source)
 
 	buildFakeArgs(source, dest, 1)
-
 	main()
+
+	if _, err := os.Stat(filepath.Join(dest, file1)); os.IsNotExist(err) {
+		t.Errorf("Couldn't find copied file %v in destination path %v", file1, dest)
+	}
 
 	os.Remove(source)
 	os.Remove(dest)
